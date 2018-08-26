6c5804bf936d93aef5f40a6de3cdcc2dbb9e8320
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-08-26 15:48:54 +0200

    add burgeringriedients component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
+
+const burger = () => {
+ return <BurgerIngredient type={'meat'}/>
+}
+
+export default burger;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import style from './Layout.css';
 
 const layout = (props) => (
     <React.Fragment>
         <div>Toolbar</div>
-        <main>
+        <main className={style.Content}>
             {props.children}
         </main>
     </React.Fragment>
