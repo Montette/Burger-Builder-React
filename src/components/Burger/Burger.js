ef2447e6d6538fd749646cc0ed5d2901391a9737
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-08-26 16:07:09 +0200

    add burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+import style from './Burger.css';
 
-const burger = () => {
- return <BurgerIngredient type={'meat'}/>
+const burger = (props) => {
+ return (
+     <div className={style.Burger}>
+        <BurgerIngredient type={'bread-top'}/>
+        <BurgerIngredient type={'cheese'}/>
+        <BurgerIngredient type={'meat'}/>
+        <BurgerIngredient type={'bread-bottom'}/>
+      </div>
+ )
 }
 
 export default burger;
